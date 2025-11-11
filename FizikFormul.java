ab7c4c759d77943379aebcb5e1624ca8173b3989
OmerEnsarshn <250542022@firat.edu.tr>
2025-11-11 12:16:05 +0300

    Update student information in FizikFormul.java

--- FizikFormul.java
+++ FizikFormul.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: Ömer Ensar ŞAHİN
+ * Ogrenci No: 250542022
+ * Tarih: 11.11.2025
  * Aciklama: Gorev 2 - Fizik Formulu Asistani
  *
  * Bu program temel fizik formullerini kullanarak
